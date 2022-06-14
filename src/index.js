f551d99564cabc35e143b2b1e0ed9cea67b50ba7
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-06-14 19:13:03 +0300

    commit tree 92ec0381db467e3a387b2d67ed3cd68f94069fd7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    //selects the form
+    const inputForm = document.querySelector('form');
+    //add eventlistener to form
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        //select the input
+        const input = document.querySelector('input#searchByID');
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+          
+    });
+
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
